0f97b0793500254f1628d6d2aa01c955b0dcfa7e
mz <mzarev1@gmai.com>
2020-09-23 21:58:13 +0100

    cleanup, plain euc done

--- build/CMakeFiles/cv_test.dir/DependInfo.cmake
+++ build/CMakeFiles/cv_test.dir/DependInfo.cmake
@@ -4,38 +4,28 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/mariyan/Documents/dev/segs/seg.cpp" "/home/mariyan/Documents/dev/segs/build/CMakeFiles/cv_test.dir/seg.cpp.o"
+  "/home/mz/Documents/dev/LidarSegment/mincut.cpp" "/home/mz/Documents/dev/LidarSegment/build/CMakeFiles/cv_test.dir/mincut.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_CXX
   "DISABLE_LIBUSB_1_0"
+  "DISABLE_OPENNI"
+  "DISABLE_OPENNI2"
   "DISABLE_PCAP"
   "DISABLE_PNG"
-  "QT_CORE_LIB"
-  "QT_GUI_LIB"
-  "QT_NO_DEBUG"
-  "QT_WIDGETS_LIB"
   "qh_QHpointer"
-  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL)"
-  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL)"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL2)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
   )
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/include/vtk-6.3"
+  "/usr/include/vtk-7.1"
   "/usr/include/freetype2"
-  "/usr/include/x86_64-linux-gnu"
-  "/usr/local/include/pcl-1.10"
+  "/usr/include/pcl-1.10"
   "/usr/include/eigen3"
-  "/usr/include/ni"
-  "/usr/include/openni2"
-  "/usr/include/x86_64-linux-gnu/qt5"
-  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
-  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
-  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
-  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
   )
 
 # Targets to which this target links.
--- build/CMakeFiles/cv_test.dir/cmake_clean.cmake
+++ build/CMakeFiles/cv_test.dir/cmake_clean.cmake
@@ -1,7 +1,7 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/cv_test.dir/seg.cpp.o"
-  "cv_test.pdb"
+  "CMakeFiles/cv_test.dir/mincut.cpp.o"
   "cv_test"
+  "cv_test.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
